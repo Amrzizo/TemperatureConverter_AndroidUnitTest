022ed6324471dc73d9bbec664e5d7e9f458fbdc2
3amr.abdelaziz@gmail.com <3amr.abdelaziz@gmail.com>
2019-07-17 12:24:54 +0200

    first Commit

--- app/src/main/java/com/amr/codes/temperatureconverter/views/MainActivity.java
+++ app/src/main/java/com/amr/codes/temperatureconverter/views/MainActivity.java
@@ -1,7 +1,9 @@
-package com.amr.codes.temperatureconverter;
+package com.amr.codes.temperatureconverter.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.amr.codes.temperatureconverter.R;
 
 public class MainActivity extends AppCompatActivity {
 
